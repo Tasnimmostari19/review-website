23df8416ffbf8b93fe069b3d7ce390462fdfbfd9
Tasnim <tasnim.mostari@gmail.com>
2021-10-04 19:52:21 +0600

    user added

--- src/Components/User/User.js
+++ src/Components/User/User.js
@@ -1,9 +1,46 @@
 import React from 'react';
+import { FormControl, InputGroup } from 'react-bootstrap';
+import './User.css'
 
 const User = () => {
     return (
         <div>
-            <h5>User info</h5>
+            <h2>User Info</h2>
+            <div className='input'>
+                <InputGroup className="mb-3 w-50">
+                    <FormControl
+                        placeholder="username"
+                        aria-label="Recipient's username"
+                        aria-describedby="basic-addon2"
+                    />
+                    <InputGroup.Text id="basic-addon2">Username</InputGroup.Text>
+                </InputGroup>
+                <InputGroup className="mb-3 w-50">
+                    <FormControl
+                        placeholder="Name"
+                        aria-label="Recipient's username"
+                        aria-describedby="basic-addon2"
+                    />
+                    <InputGroup.Text id="basic-addon2">Name</InputGroup.Text>
+                </InputGroup>
+                <InputGroup className="mb-3 w-50">
+                    <FormControl
+                        placeholder="Email"
+                        aria-label="Recipient's username"
+                        aria-describedby="basic-addon2"
+                    />
+                    <InputGroup.Text id="basic-addon2">Email</InputGroup.Text>
+                </InputGroup>
+                <InputGroup className="mb-3 w-50">
+                    <FormControl
+                        placeholder="Phone"
+                        aria-label="Recipient's username"
+                        aria-describedby="basic-addon2"
+                    />
+                    <InputGroup.Text id="basic-addon2">Phone</InputGroup.Text>
+                </InputGroup>
+
+            </div>
         </div>
     );
 };
